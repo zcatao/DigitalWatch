00a435d656a762c784885e2d767ca4ddd2c1cd03
zcatao <yangsontao19950616@vip.qq.com>
2016-01-15 11:13:57 +0800

    a

--- watch_cnt/watch_cnt.v
+++ watch_cnt/watch_cnt.v
@@ -1,27 +1,64 @@
 /*****************************************
-模块名：clk_div
-编写者：zcataox
-编写时间：2016年1月14日21:23:23
+模块名：watch_cnt
+编写者：zcatao
+编写时间：2016年1月15日 10:31:36
 输入：
-    50MHZ时钟源clk_50Mhz
+    1Khz时钟源clk_1Khz
     异步复位信号rst
+    来自键盘处理的命令
 输出：
-    1KHZ时钟信号clk_1Khz
+    送给显示模块的数据缓存dispbuf
 注意事项：
-    默认rst复位信号高电平有效，若为低电平有效记得修改
-    如果输入输出频率比有变化，可以修改clk_div实现
+
 ***********************************************/
+
 module watch_cnt (
     input clk_1Khz,
     input rst,
-    input pause,
-    output[23:0] dispbuf
+    input [3:0]Command,
+    output [23:0] dispbuf
     );
+    reg [3:0] minute1;
+    reg [3:0] minute0;
+    reg [3:0] sec1;
+    reg [3:0] sec0;
+    reg [3:0] msec2;
+    reg [3:0] msec1;
+    reg [3:0] msec0;
+    //reg [35:0] Time;
+    reg [6:0] cout; // 代表七位数字的进位
+    //毫秒个位
+    always @ (posedge clk_1Khz or posedge rst) begin
+      if (rst) begin
+          msec0 <= 0;
+          cout <= 0;
+      end else if (msec0 == 4'd9) begin
+          msec0 <= 0;
+          cout[0] <= 1;
+      end else begin
+          msec0 <= msec0 + 1;
+          cout[0] < = 0;
+      end
+    end
+    //毫秒十位
+    always @ (posedge cout[0] or posedge rst) begin
+        if(rst) begin
+            msec1 <= 0;
+            cout[1] <= 0;
+        end else if (msec1 == 4'd9) begin
+            msec1 <= 0;
+            cout[1] <= 1;
+        end else begin
+            msec1 <= msec1 + 1;
+            cout[1] <= 0;
+        end
+    end
 
-    reg[:0] hour;
-    reg [3:0] minute
-    reg [3:0] sec;
-    reg [3:right_range] name;
+
+
+
+
+
 
 
 
