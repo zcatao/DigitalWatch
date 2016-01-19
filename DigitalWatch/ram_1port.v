365ca4b4d8f2bc99d092df441ee7adea883ec5cf
zcatao <yangsontao19950616@vip.qq.com>
2016-01-19 21:10:18 +0800

    ram

---
+++ DigitalWatch/ram_1port.v
@@ -0,0 +1,21 @@
+
+
+
+module ram_1port(
+    input [3:0] address;
+    input       clk,
+    input [23:0]data,
+    input       wren,
+    output[23:0]q
+    )
+
+    reg[23:0] mem[7:0];
+
+    always @ (posedge clk) begin
+        if(wren)
+            mem[address] <= data;
+    end
+
+    assign q = mem[address];
+
+endmodule
